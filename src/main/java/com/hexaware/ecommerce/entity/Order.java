68f8813b2d694cd4785d851ce05b37de83e1bd98
Rachakonda-Sreeram <sriraam.raachaakondaa@gmail.com>
2024-02-03 13:09:22 +0530

    Made few changes

--- src/main/java/com/hexaware/ecommerce/entity/Order.java
+++ src/main/java/com/hexaware/ecommerce/entity/Order.java
@@ -8,12 +8,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 @Entity
+@Table(name="OrderTable")
 public class Order {
 	@Id
 	private int orderId;        // Primary Key
